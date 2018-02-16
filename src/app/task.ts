1e8a6493fba5423d28e29ef60875899aacdd09f3
Jason Guzik <jguzik@gmail.com>
2018-02-16 17:07:45 -0500

    removed task class

--- src/app/task.ts
+++
@@ -1,8 +0,0 @@
-export class Task {
-	id: number;
-	description: string;
-	completed: boolean;
-	editable: boolean;
-}
-
-// I would like to add a due date at some point, with the functionality to add completed date and time when clicked on finish
